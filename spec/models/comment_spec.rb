5d998bd7a5d994596a044e06a477bca5ef6941bd
miroslav <miroslav@kolosek.com>
2023-05-04 11:18:22 +0200

    Add Comments count test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Comment, type: :model do
   it do
     expect(Comment.count).to be 0
   end
+
+  it do
+    article = Article.create(title: 'article 1', body: 'article 1', status: 'public')
+    comment = create(:comment, article_id: article.id)
+
+    expect(Comment.count).to eq 1
+  end
 end
